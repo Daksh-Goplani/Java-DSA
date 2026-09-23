7e9482700198f88bc6112a36aae17ab32d77eda9
Daksh Goplani <dakshgoplani7@gmail.com>
2026-09-23 15:54:14 +0530

    Leetcode: 28 & 709

---
+++ 11. Leetcode/11.2 Strings/0028.IndexFirstOccurString.java
@@ -0,0 +1,16 @@
+// class Solution {
+//     public int strStr(String haystack, String needle) {
+//         for(int i = 0; i<haystack.length(); i++){
+//             if(haystack.substring(i).startsWith(needle)){
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
